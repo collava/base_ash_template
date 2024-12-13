4ea1f3504ac06ab95bccddfd64d8445fdd1585bf
Renê W Uemura Schneider <renews@gmail.com>
2024-12-13 15:19:44 +0100

    Refactor configuration for clarity and organization
    - Consolidated and reorganized application configuration in `config/config.exs` for improved readability.
    - Updated `ash` configuration to use `Money` type directly from `AshMoney.Types`.
    - Added `esbuild` configuration for asset bundling and specified build arguments.
    - Configured `logger` settings for better log formatting and metadata inclusion.
    - Set up `reet` configurations for mailer, endpoint, and Oban queues.
    - Ensured `mime` and `phoenix` JSON library settings are correctly defined.
    - Removed redundant configurations to streamline the setup.

--- config/config.exs
+++ config/config.exs
@@ -7,18 +7,66 @@
 # General application configuration
 import Config
 
-config :mime,
-  extensions: %{"json" => "application/vnd.api+json"},
-  types: %{"application/vnd.api+json" => ["json"]}
-
-config :ex_cldr, default_backend: Reet.Cldr
+alias AshMoney.Types.Money
 
 config :ash,
   include_embedded_source_by_default?: false,
   default_page_type: :keyset,
   policies: [no_filter_static_forbidden_reads?: false],
-  known_types: [AshMoney.Types.Money],
-  custom_types: [money: AshMoney.Types.Money]
+  known_types: [Money],
+  custom_types: [money: Money]
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.17.11",
+  reet: [
+    args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    # Build-time config (config/{config, dev, test, prod}.exs)
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+config :ex_cldr, default_backend: Reet.Cldr
+
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+config :mime,
+  extensions: %{"json" => "application/vnd.api+json"},
+  types: %{"application/vnd.api+json" => ["json"]}
+
+config :phoenix, :json_library, Jason
+
+config :reet, Oban,
+  queues: [default: 10, mailers: 20, events: 50, media: 5, imports: 40],
+  repo: Reet.Repo
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :reet, Reet.Mailer, adapter: Swoosh.Adapters.Local
+
+config :reet, ReetWeb.Endpoint,
+  url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [html: ReetWeb.ErrorHTML, json: ReetWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: Reet.PubSub,
+  live_view: [signing_salt: "PIXqUKf8"]
+
+config :reet,
+  ash_domains: [Reet.Accounts, Reet.Blog]
+
+config :reet,
+  ecto_repos: [Reet.Repo],
+  generators: [timestamp_type: :utc_datetime]
 
 config :spark,
   formatter: [
@@ -50,46 +98,6 @@ config :spark,
     ]
   ]
 
-config :reet,
-  ash_domains: [Reet.Accounts, Reet.Blog]
-
-config :reet,
-  ecto_repos: [Reet.Repo],
-  generators: [timestamp_type: :utc_datetime]
-
-config :reet, ReetWeb.Endpoint,
-  url: [host: "localhost"],
-  adapter: Bandit.PhoenixAdapter,
-  render_errors: [
-    formats: [html: ReetWeb.ErrorHTML, json: ReetWeb.ErrorJSON],
-    layout: false
-  ],
-  pubsub_server: Reet.PubSub,
-  live_view: [signing_salt: "PIXqUKf8"]
-
-# Configures the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :reet, Reet.Mailer, adapter: Swoosh.Adapters.Local
-
-config :reet, Oban,
-  queues: [default: 10, mailers: 20, events: 50, media: 5, imports: 40],
-  repo: Reet.Repo
-
-# Configure esbuild (the version is required)
-config :esbuild,
-  version: "0.17.11",
-  reet: [
-    args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
-    cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
-
 # Configure tailwind (the version is required)
 config :tailwind,
   version: "3.4.3",
@@ -102,10 +110,11 @@ config :tailwind,
     cd: Path.expand("../assets", __DIR__)
   ]
 
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+config :tower, :reporters, [TowerEmail]
 
-config :phoenix, :json_library, Jason
+config :tower_email,
+  otp_app: :reet,
+  from: {"Errors", "no-reply@reet.com"},
+  to: "monitoring@reet.com"
 
 import_config "#{config_env()}.exs"
