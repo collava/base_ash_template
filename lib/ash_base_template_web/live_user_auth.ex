4dc34d15c8ed87e55698c92940a55b938efd886f
Renê W Uemura Schneider <renews@gmail.com>
2024-12-16 18:08:49 +0100

    Refactor admin access control in LiveUserAuth module
    - Updated the `on_mount` function to enhance admin-only access control by retrieving the user from the session and checking their role.
    - Implemented user lookup by ID to ensure proper access management for admin users.
    - Redirects non-admin users to the sign-in page, improving security for restricted routes.
    
    These changes strengthen the security measures in place for user authentication and access control within the application.

--- lib/ash_base_template_web/live_user_auth.ex
+++ lib/ash_base_template_web/live_user_auth.ex
@@ -7,6 +7,8 @@ defmodule AshBaseTemplateWeb.LiveUserAuth do
 
   import Phoenix.Component
 
+  alias AshBaseTemplate.Accounts
+
   def on_mount(:live_user_optional, _params, _session, socket) do
     if socket.assigns[:current_user] do
       {:cont, socket}
@@ -31,11 +33,19 @@ defmodule AshBaseTemplateWeb.LiveUserAuth do
     end
   end
 
-  def on_mount(:admins_only, _params, _session, socket) do
+  def on_mount(:admins_only, _params, %{"user" => user} = _session, socket) do
     if socket.assigns[:current_user] && socket.assigns[:current_user].role == :admin do
       {:cont, socket}
     else
-      {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/sign-in")}
+      user_id = user |> String.split("?id=") |> List.last()
+      user = Accounts.get_user!(user_id)
+      socket = assign(socket, current_user: user)
+
+      if user.role == :admin do
+        {:cont, socket}
+      else
+        {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/sign-in")}
+      end
     end
   end
 end
