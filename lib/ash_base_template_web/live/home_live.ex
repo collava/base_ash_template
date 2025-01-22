80eba139e21f4e516320eb1894ae8bcc1c822746
Renê Schneider <53726+renews@users.noreply.github.com>
2025-01-22 02:47:57 +0100

    new home

--- lib/ash_base_template_web/live/home_live.ex
+++ lib/ash_base_template_web/live/home_live.ex
@@ -17,7 +17,9 @@ defmodule AshBaseTemplateWeb.HomeLive do
           Ash Base Template
         </p>
         <p class="mt-4 text-base leading-7 text-zinc-600">
-          Dont forget to read the README.md file
+          Dont forget to read the README.md file.<br />
+          You can check the terminal for links send to the email you provided.<br />
+          You can use any email, there's no live sending in development.
         </p>
         <div class="mt-8 max-w-md mx-auto">
           <.live_component module={AshBaseTemplateWeb.Components.NewsletterForm} id="newsletter-form" />
@@ -25,7 +27,7 @@ defmodule AshBaseTemplateWeb.HomeLive do
 
         <div class="flex">
           <div class="w-full sm:w-auto">
-            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-3">
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
               <a
                 href={~p"/posts"}
                 class="group relative rounded-2xl px-6 py-4 text-sm font-semibold leading-6 text-zinc-900 sm:py-6"
@@ -33,7 +35,55 @@ defmodule AshBaseTemplateWeb.HomeLive do
                 <span class="absolute inset-0 rounded-2xl bg-zinc-50 transition group-hover:bg-zinc-100 sm:group-hover:scale-105">
                 </span>
                 <span class="relative flex items-center gap-4 sm:flex-col">
-                  <.icon name="hero-document-text" class="h-6 w-6" /> List Posts Example
+                  <.icon name="hero-document-text" class="h-6 w-6" /> List Posts
+                </span>
+              </a>
+              <a
+                href={~p"/admin"}
+                target="_blank"
+                class="group relative rounded-2xl px-6 py-4 text-sm font-semibold leading-6 text-zinc-900 sm:py-6"
+              >
+                <span class="relative flex items-center gap-4 sm:flex-col">
+                  <.icon name="hero-cog" class="h-6 w-6" /> Admin
+                </span>
+              </a>
+              <a
+                href={~p"/admin/mailbox"}
+                target="_blank"
+                class="group relative rounded-2xl px-6 py-4 text-sm font-semibold leading-6 text-zinc-900 sm:py-6 text-center"
+              >
+                <span class="relative flex items-center gap-4 sm:flex-col">
+                  <.icon name="hero-envelope" class="h-6 w-6" /> Mailbox
+                </span>
+              </a>
+              <a
+                href={
+                  ~p"/admin?domain=Notifiers&resource=Newsletter&table=&action_type=read&action=read"
+                }
+                target="_blank"
+                class="group relative rounded-2xl px-6 py-4 text-sm font-semibold leading-6 text-zinc-900 sm:py-6"
+              >
+                <span class="relative flex items-center gap-4 sm:flex-col text-center">
+                  <.icon name="hero-envelope-solid" class="h-6 w-6" /> All Newsletter Signups
+                </span>
+              </a>
+              <a
+                href={~p"/admin/errors"}
+                target="_blank"
+                class="group relative rounded-2xl px-6 py-4 text-sm font-semibold leading-6 text-zinc-900 sm:py-6"
+              >
+                <span class="relative flex items-center gap-4 sm:flex-col">
+                  <.icon name="hero-bug-ant" class="h-6 w-6" /> Error Tracking
+                </span>
+              </a>
+
+              <a
+                href={~p"/api/swaggerui"}
+                target="_blank"
+                class="group relative rounded-2xl px-6 py-4 text-sm font-semibold leading-6 text-zinc-900 sm:py-6"
+              >
+                <span class="relative flex items-center gap-4 sm:flex-col">
+                  <.icon name="hero-code-bracket" class="h-6 w-6" /> Swagger API
                 </span>
               </a>
             </div>
