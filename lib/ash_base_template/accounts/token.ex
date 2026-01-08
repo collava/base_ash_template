79c2340486c476c500e932539df0f83a89f5925d
Rene Schneider <53726+renews@users.noreply.github.com>
2026-01-08 13:55:56 -0300

    fix(ash_auth): apply necessary changes

--- lib/ash_base_template/accounts/token.ex
+++ lib/ash_base_template/accounts/token.ex
@@ -30,10 +30,10 @@ defmodule AshBaseTemplate.Accounts.Token do
       prepare AshAuthentication.TokenResource.GetTokenPreparation
     end
 
-    action :revoked? do
+    action :revoked?, :boolean do
       description "Returns true if a revocation token is found for the provided token"
-      argument :token, :string, sensitive?: true, allow_nil?: false
-      argument :jti, :string, sensitive?: true, allow_nil?: false
+      argument :token, :string, sensitive?: true
+      argument :jti, :string, sensitive?: true
 
       run AshAuthentication.TokenResource.IsRevoked
     end
